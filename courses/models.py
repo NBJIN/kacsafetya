13b690fec8ecf6bebe7d3b494b2dcf868d7dba26
noreen <noreennelligancilms@gmail.com>
2023-04-21 22:05:04 +0000

    Register courses app
    Models.py add code to correct plural name of courses in
    admin.py, register courses path in courses url.py file,
    and register in the project level urls. Create view
    for the courses app and add template for courses page.

--- courses/models.py
+++ courses/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Courses(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Courses"
     details = models.CharField(max_length=5000)
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Courses
 
-# Create your views here.
+
+def courses(request):
+    """ A view to show list of all courses """
+
+    # course = Courses.details.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
