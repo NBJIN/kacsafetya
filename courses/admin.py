c77ecec3389910a13c79b60ed5999d83ce8065ef
noreen <noreennelligancilms@gmail.com>
2023-04-23 11:12:46 +0000

    Customised display of models in admin.py
    Customised the display of courses, location and group_by
    in the admin.py file

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Courses, Location, Group_By
 
 
-admin.site.register(Courses)
-admin.site.register(Location)
-admin.site.register(Group_By)
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'location', 'group_by', 'fee',
+    )
+
+    ordering = ('title',)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class Group_ByAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Group_By, Group_ByAdmin)
