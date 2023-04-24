d227e99f1423b0e4803ba6198c7424f8079c67f2
noreen <noreennelligancilms@gmail.com>
2023-04-24 17:44:04 +0000

    Updated Static files as issue with deployment

--- kac_safety/settings.py
+++ kac_safety/settings.py
@@ -162,8 +162,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinary Storage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # from tutor assistance
 
 
 MEDIA_URL = '/media/'
