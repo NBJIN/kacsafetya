bdfe45810217ddfd9056f04b6f05d97f9b720eb8
noreen <noreennelligancilms@gmail.com>
2023-04-25 13:10:22 +0000

    Add serach criteria to search button on nav bar
    Add search cirteria to search box on the main nav
    bar and add code for group_by and location in
    all_courses page

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
+from django.contrib import messages
 from .models import Courses, Location, Group_By
 
 
@@ -6,9 +8,28 @@ def all_courses(request):
     """ A view to show list of all courses """
 
     courses = Courses.objects.all()
+    lookup = None
+
+    if request.GET:
+        if 'group_by' in request.GET:
+            group_by = request.GET['group_by'].split(',')
+            courses = courses.filter(group_by__name__in=group_by)
+            group_by = Group_By.objects.filter(name__in=group_by)
+
+        # lookup is not working when i enter a word that is not on the website 
+        if 'lookup' in request.GET:
+            lookup = request.GET['lookup']
+            if not lookup:
+                messages.error(request, "No details entered!")
+                return redirect(reverse('landing'))
+
+            lookup = Q(title__icontains=lookup) | Q(details__icontains=lookup)
+            courses = courses.filter(lookup)
 
     context = {
         'courses': courses,
+        'search_term': lookup,
+        'group': group_by,
     }
 
     return render(request, 'courses/all_courses.html', context)
@@ -20,6 +41,7 @@ def detailed_courses(request, course_id):
 
     context = {
         'courses': courses,
+
     }
 
     return render(request, 'courses/detailed_courses.html', context)
