028ada47ab2163fa99267c0f8dd61a28d8e20a51
noreen <noreennelligancilms@gmail.com>
2023-04-29 15:06:08 +0000

    Replace comma in context.py context to a colon
    Replace comma with colon as comma was not being accepted
    Defined total, free_discount and discount at the top of the
    contexts.py

--- basket/contexts.py
+++ basket/contexts.py
@@ -6,24 +6,29 @@ def basket_contents(request):
 
     basket_items = []
     grand_total = 0
+    total = 0
     course_count = 0
+    FREE_DISCOUNT = 0
+    discount = 0
 
-    if grand_total < settings.DISCOUNT:
+    if total < settings.DISCOUNT:
         grand_total = total * Decimal(settings.STANDARD_DISCOUNT_PERCENTAGE*10/100)
-        free_discount_delta = FREE_DISCOUNT - total 
-    else:
-        discount = 0
-        free_discount_delta = 0
+        free_discount_delta = FREE_DISCOUNT - total
+    free_discount_delta = FREE_DISCOUNT - total
+ 
+    # else:
+    #     discount = 0
+    #     free_discount_delta = 0
 
     grand_total = discount - total
 
     context = {
-        'basket_items', basket_items,
-        'grand_total', grand_total,
-        'course_count', course_count,
-        'settings.DISCOUNT', settings.DISCOUNT,
-        'free_discount_delta', free_discount_delta,
-        'settings.STANDARD_DISCOUNT_PERCENTAGE', settings.STANDARD_DISCOUNT_PERCENTAGE,
-        }
+        'basket_items': basket_items,
+        'grand_total': grand_total,
+        'course_count': course_count,
+        'settings.DISCOUNT': settings.DISCOUNT,
+        'free_discount_delta': free_discount_delta,
+        'settings.STANDARD_DISCOUNT_PERCENTAGE': settings.STANDARD_DISCOUNT_PERCENTAGE,
+    }
 
     return context
