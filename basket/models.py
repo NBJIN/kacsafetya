1585fa8f20bfd95147c4c10c678cde2ce468be2a
noreen <noreennelligancilms@gmail.com>
2023-04-29 16:35:47 +0000

    Added basket models to the models.py file

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Basket(models.Model):
+    title = models.CharField(max_length=250)
+    quantity = models.PositiveIntegerField(default=0)
+    fee = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    discount = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.course_title
