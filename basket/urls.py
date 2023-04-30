7637950f1859be891912dda90e0c333f8f2bc2ca
noreen <noreennelligancilms@gmail.com>
2023-04-30 09:36:51 +0000

    Update url for view basket in basket urls
    Add back in link to view the basket in the base.html page

--- basket/urls.py
+++ basket/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, reverse
 from . import views
-from .views import add_to_basket, view_basket
+# from .views import add_to_basket, view_basket
 
 
-app_name = 'basket'
+# app_name = 'basket'
 
 
 urlpatterns = [
-    path('basket/<int:item_id>/', views.view_basket, name='view_basket'),
+    path('', views.view_basket, name='view_basket'),
     path('add/<int:item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
