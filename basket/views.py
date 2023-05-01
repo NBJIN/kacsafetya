53d76eda46ebb40fc2202c9252746aab019a9c23
noreen <noreennelligancilms@gmail.com>
2023-05-01 23:28:18 +0000

    Add messages to basket views
    Messages added to basket views.py are success messages and
    are added to each view.  In base html corrected eorror in
    message-container code had omitted a full stop

--- basket/views.py
+++ basket/views.py
@@ -17,16 +17,17 @@ def add_to_basket(request, item_id):
     """ Add a quantity of courses to the basket """
     courses = Courses.objects.get(pk=item_id)
 
-    # course = get_object_or_404(Courses, pk=item_id)
+    course = get_object_or_404(Courses, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {basket.title} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
-        messages.success(request, f'Added {courses.title} to your basket')
+        messages.success(request, f'Added {basket.title} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -35,22 +36,26 @@ def add_to_basket(request, item_id):
 def update_basket(request, item_id):
     """ update the quantity of a course to the correct amount  """
 
-    # course = get_object_or_404(Courses, pk=item_id)
+    courses = get_object_or_404(Courses, pk=item_id)
     quantity = int(request.POST.get('quantity'))
 
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Updated {basket.title} quantity to {basket[item_id]}')
     elif quantity == 0:
         del basket[item_id]
         if not basket[item_id]:
             bag.pop(item_id)
+            messages.success(request, f'Successfully deleted {basket.title} from your basket')
     else:
         if quantity > 0:
             basket[item_id] = quantity
+            messages.success(request, f'updated {basket.title} quantity to {basket[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Successfully removed {basket.title} from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -60,16 +65,20 @@ def delete_from_basket(request, item_id):
     """ Delete an item from basket  """
 
     try:
+        courses = get_object_or_404(Courses, pk=item_id)
         basket = request.session.get('basket', {})
 
         del basket[item_id]
         if not basket[item_id]:
-            bag.pop(item_id)
+                bag.pop(item_id)
+                messages.success(request, f'Successfully removed {basket.title} from your basket')
         else:
             bag.pop[item_id]
+            messages.success(request, f'Successfully removed {basket.title} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
