7ff9487dbb1222a638ca65c94ae216363344fb84
noreen <noreennelligancilms@gmail.com>
2023-05-10 23:27:32 +0000

    Purchase views add stripe keys and secret
    Purchase.css add style for layout of form on purchase page
    Add js for stripe in stripe_elements.js file
    Add postloadjs block to purchase.html.  change headling on
    line 27 in purchase.html also correct spelling in line 119.
    Base.html load strip in the blockcorejs block

--- purchase/views.py
+++ purchase/views.py
@@ -14,6 +14,8 @@ def purchase(request):
     template = 'purchase/purchase.html'
     context = {
         'purchase_form': purchase_form,
+        'stripe_public_key': 'pk_test_51MkRbADXf5h0wE6pVKuwXnUgvGX4IJilzaVpBltzeFkJBEc6Bam50b7t9xto2u6G4tYbbOHnebMKIgPj1NEc71HY00KCkNss9a',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
