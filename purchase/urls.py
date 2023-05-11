ded775c843a80c3e16173da5e740d3031d0a0577
noreen <noreennelligancilms@gmail.com>
2023-05-11 23:14:02 +0000

    Add default_app_config to purchase init.py
    In purchase models line 34 added or 0 to the end of the line
    of committed
    In purchase urls added url for purchase_success
    Added view for purchase and purchase success
    Add template for purchase_success

--- purchase/urls.py
+++ purchase/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.purchase, name='purchase')
+    path('', views.purchase, name='purchase'),
+    path('purchase_success/<purchase_no>', views.purchase_success, name='purchase_success'),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import PurchaseForm
+from .models import Purchase, PurchaseOrderItem
+from courses.models import Courses
 from basket.contexts import basket_contents
 
 import stripe
@@ -12,24 +14,65 @@ def purchase(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "No items in your bag at present")
-        return redirect(reverse('all_courses'))
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    current_basket = basket_contents(request)
-    total = current_basket['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY, 
+        form_data = {
+            'fname': request.POST['fname'],
+            'lname': request.POST['lname'],
+            'company': request.POST['company'],
+            'address1': request.POST['address1'],
+            'address2': request.POST['address2'],
+            'address3': request.POST['address3'],
+            'postcode': request.POST['postcode'],
+            'telephone': request.POST['telephone'],
+            'email': request.POST['email'],
+            'course_title': request.POST['course_title'],
+            'quantity': request.POST['quantity'],
+        }
+        purchase_form = PurchaseForm(form_data)
+        if purchase_form.is_valid():
+            purchase = purchase_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    courses = Courses.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        purchase_order_item = PurchaseOrderItem(
+                            purchase_no=purchase_no,
+                            courses=courses,
+                            quantity=item_data,
+                        )
+                        purchase_order_item.save()
+                    else:
+                        purchase_order_item.save()
+                except Courses.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your basket wasn't found in our database. "
+                        "Make contact for assistance")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+            request.sesstion['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('purchase_success', args[purchase.purchase_no]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+            Please check your details.')
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "No items in your bag at present")
+            return redirect(reverse('all_courses'))
 
-    )
+        current_basket = basket_contents(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY, 
 
-    print(intent)
-
-    purchase_form = PurchaseForm()
+        )
+        purchase_form = PurchaseForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -39,6 +82,27 @@ def purchase(request):
         'purchase_form': purchase_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def purchase_success(request, purchase_no):
+    """
+    view to return a successful purchase
+    """
+    save_info = request.session.get('save_info')
+    purchase = get_object_or_404(Purchase, purchase_no=purchase_no)
+    messages.success(request, f'Purchase succfully processed \
+        Your purchase order number is {purchase_no}. Confirmation \
+        of your purchase has been sent to {purchase.email}')
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'purchase/purchase_success.html'
+    context = {
+        'purchase': purchase,
     }
 
     return render(request, template, context)
