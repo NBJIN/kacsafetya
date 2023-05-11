b52444d02ba69ae1514d1c23645c82ca68b66536
noreen <noreennelligancilms@gmail.com>
2023-05-11 11:51:13 +0000

    Remove space in stripe.js and correct postloadjs script tag in purchase.html

--- purchase/static/purchase/js/stripe_elements.js
+++ purchase/static/purchase/js/stripe_elements.js
@@ -2,7 +2,6 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
-
 var style = {
     base: {
         color: '#000',
@@ -19,4 +18,5 @@ var style = {
     }
 };
 var card = elements.create('card', {style: style});
+
 card.mount('#card-element');
