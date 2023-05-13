a4d318fcc27ea1e442287ce6db5e869d897f2274
noreen <noreennelligancilms@gmail.com>
2023-05-13 22:44:41 +0000

    Add line to end of stripe_elements.js in purchases
    In purchase.css added styling for #card-element
    Add code to wehbhook_handler.py of payment intent succeeded

--- purchase/webhook_handler.py
+++ purchase/webhook_handler.py
@@ -20,7 +20,58 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        basket = intent.metadata.basket
+        save_info = intent.metadata.save_info
+        # need to check the following code 
+        customer_details = intent.charges.data[0].customer_details
+        contact_details = intent.contact
+        grand_total = round(intent.data.charges[0].amount / 100, 2)
+
+        # Clean data in the shipping details - need to check the following code 
+        for field, value in contact_details.address.items():
+            if value == "":
+                contact_details.address[field] = None 
+
+        purchase_exists = False
+        try:
+            purchase = Purchase.objects.get(
+                fname_name__iexact=contact_details.fname,
+                lname_name__iexact=contact_details.lname,
+                company_name__iexact=contact_details.company,
+                address1_iexact=contact_details.address1,
+                address2_iexact=contact_details.address2,
+                address3_iexact=contact_details.address3,
+                postcode__iexact=contact_details.postcode,
+                telephone__iexact=contact_details.telephone,
+                email__iexact=contact_details.email,
+            )
+            purchase_exists = True
+            return HttpResponse(
+                content=f'Webhook recieved: {event["type"]} | SUCCESS: Verified order already in the database',
+                status=200)
+        except Purchase.DoesNotExist:
+               for item_id, item_data in basket.items():
+                try:
+                    courses = Courses.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        purchase_order_item = PurchaseOrderItem(
+                            # purchase_no=purchase_no,
+                            purchase=purchase,
+                            courses=courses,
+                            quantity=item_data,
+                        )
+                        purchase_order_item.save()
+                  
+
+
+
+
+
+
+
+
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
