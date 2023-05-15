38ee39432848fdf38b794f540c3efd96eb1049ed
noreen <noreennelligancilms@gmail.com>
2023-05-15 12:15:44 +0000

    Add urls for dashboard to dashboard urls.py
    Add view for dashboard to dashboard views.py
    Add dashboard css fill and html page to render
    the dashboard page.  Add url to project urls and
    change name of Proile on base template to dashboard.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    """
+    Display the users dashboard
+    """
+    template = 'dashboard/dashboard.html'
+    context = {}
+
+    return render(request, template, context)
--- kac_safety/urls.py
+++ kac_safety/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('courses/', include('courses.urls')),
     path('basket/', include('basket.urls')),
     path('purchase/', include('purchase.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
