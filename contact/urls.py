53923d10a3e204a4ba8ffe08fae60891dca26ef0
noreen <noreennelligancilms@gmail.com>
2023-05-23 23:05:57 +0000

    Work on getting contact form submitted

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import contact_approved
+from .views import contact_approved, contact
 
 
 urlpatterns = [
-    path('', views.view_contact, name='view_contact'),
-    path('contact/approved', views.contact_approved, name='contact_approved'),
+    path('contact/', views.contact, name='contact'),
+    path('contact_approved/', views.contact_approved, name='contact_approved'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from .forms import ContactForm
+from .models import Contact
 
 
-def view_contact(request):
+def contact(request):
     """ A view to return the contact page """
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -16,15 +17,48 @@ def view_contact(request):
             # 'date_added': 'Date',
             course_title = form.cleaned_data['course_title']
             message = form.cleaned_data['message']
-            return redirect('contact_approved')
+            form.save()
+            return redirect('contact.html')
+
     else:
         form = ContactForm()
-
-    return render(request, 'contact/contact.html', {'form': form})
+    return render(request, 'contact.html', {'form': form})
 
 
 def contact_approved(request):
-    """
-    A veiw to return the contact approved  page
-    """
     return render(request, 'contact/contact_approved.html')
+
+    # if request.method == 'POST':
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         fullname = form.cleaned_data['fullname']
+    #         company = form.cleaned_data['company']
+    #         telephone = form.cleaned_data['telephone']
+    #         email = form.cleaned_data['email']
+    #         # 'date_added': 'Date',
+    #         course_title = form.cleaned_data['course_title']
+    #         message = form.cleaned_data['message']
+    #         return redirect('contact_approved')
+    # else:
+    #     form = ContactForm()
+
+    # return render(request, 'contact/contact.html', {'form': form})
+
+    # if request.method == 'POST':
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         cd = form.cleaned_data
+    #         return HttpResponseRedirect('/contact?submitted=True')
+    #     else:
+    #         form = ContactForm()
+    #         if 'submitted' in request.GET:
+    #             submitted = True
+
+    #     return render(request, 'view_contact', {'form': form, 'submitted': submitted})
+
+
+# def contact_approved(request):
+#     """
+#     A veiw to return the contact approved  page
+#     """
+#     return render(request, 'contact/contact_approved.html')
