50b84809759b16a9c6130ff625979be970afe79d
noreen <noreennelligancilms@gmail.com>
2023-07-20 11:17:11 +0000

    Update allowed host name in settings
    Was not able to run file updated allowed hosts in settings.py
    circa line 34.

--- kac_safety/settings.py
+++ kac_safety/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['kac-safetya.herokuapp.com', 'localhost', '8000-nbjin-kacsafetya-c2m4cr6itw4.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['kac-safetya.herokuapp.com', 'localhost', '8000-nbjin-kacsafetya-c2m4cr6itw4.ws-eu101.gitpod.io']
 
 
 # Application definition
