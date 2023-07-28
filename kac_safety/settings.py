c952dfe9be46e8447fa732864f0384bad9429629
noreen <noreennelligancilms@gmail.com>
2023-07-28 23:51:07 +0000

    Uncomment line 192 in settings.py for static files

--- kac_safety/settings.py
+++ kac_safety/settings.py
@@ -189,7 +189,7 @@ STATIC_URL = '/static/'
 # # this was originally in project
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # ## this was originally in project
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
