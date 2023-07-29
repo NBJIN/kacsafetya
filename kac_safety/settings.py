4708b2b6590562e7f3d6ea81e7ee80769a7323fb
noreen <noreennelligancilms@gmail.com>
2023-07-29 21:22:55 +0000

    change debug status

--- kac_safety/settings.py
+++ kac_safety/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ  # True
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ  # True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['kac-safetya.herokuapp.com', 'localhost',
                  '8000-nbjin-kacsafetya-c2m4cr6itw4.ws-eu102.gitpod.io']
