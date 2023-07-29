9e825e155f91ac924ff82fa500900417ab45199a
noreen <noreennelligancilms@gmail.com>
2023-07-29 22:02:42 +0000

    Add purchase to url for webhook in purchase urls

--- purchase/urls.py
+++ purchase/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
          views.purchase_success, name='purchase_success'),
     path('cache_purchase_data/', views.cache_purchase_data,
          name='cache_purchase_data'),
-    path('wh/', webhook, name='webhook'),
+    path('purchase/wh/', webhook, name='webhook'),
 ]
