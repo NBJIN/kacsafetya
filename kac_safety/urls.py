d27980b16405a8a6caee9823ab924aab1c19ab9e
noreen <noreennelligancilms@gmail.com>
2023-11-01 22:08:29 +0000

    Update kac_safety urls and robots.txt
    Update kac_safety urls as robots.txt was showing a 404 error
    added path for robots txt file.

--- kac_safety/urls.py
+++ kac_safety/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +30,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('contact/', include('contact.urls')),
     path('mailchimpnews/', include('mailchimpnews.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt",
+         content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
